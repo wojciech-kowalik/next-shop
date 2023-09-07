fc0eaacdaa7c98b7f23954c3e180edcb25dc7d1b
Wojciech Kowalik <wkowalik81@gmail.com>
2023-09-07 15:31:02 +0200

    chore: Using next/image

--- src/app/ui/atoms/ProductCoverImage.tsx
+++ src/app/ui/atoms/ProductCoverImage.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 type Props = {
 	alt: string;
 	src: string;
@@ -6,7 +8,7 @@ type Props = {
 const ProductCoverImage = ({ alt, src }: Props) => {
 	return (
 		<div className="aspect-square overflow-hidden rounded-md border bg-slate-50 hover:bg-slate-100">
-			<img
+			<Image
 				width={320}
 				height={320}
 				className="h-full w-full object-cover object-center p-4 transition-transform hover:scale-105"
