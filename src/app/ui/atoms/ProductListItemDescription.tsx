dfd88cd6bfd135468084a1a4c54a0639eb572e4d
Wojciech Kowalik <wkowalik81@gmail.com>
2023-09-29 14:30:58 +0200

    fix: heading

--- src/app/ui/atoms/ProductListItemDescription.tsx
+++ src/app/ui/atoms/ProductListItemDescription.tsx
@@ -12,7 +12,7 @@ export default function ProductListItemDescription({
 		<div className="mt-2 flex justify-between">
 			<div>
 				<h3 className="text-sm font-semibold text-gray-700">{name}</h3>
-				<h2 className="text-sm text-gray-500">{category}</h2>
+				<p className="text-sm text-gray-500">{category}</p>
 			</div>
 			<p className="text-sm font-medium text-gray-900">
 				<span className="sr-only">Cena: </span> {formatMoney(price / 100)}
