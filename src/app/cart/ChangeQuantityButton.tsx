9374537b88bc09183596cbb0936abd1824785cef
Wojciech Kowalik <wkowalik81@gmail.com>
2023-10-17 13:31:17 +0200

    fix: changes after update

--- src/app/cart/ChangeQuantityButton.tsx
+++ src/app/cart/ChangeQuantityButton.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { experimental_useOptimistic as useOptimistic } from "react";
+import { useOptimistic } from "react";
 import { changeItemQuantityAction } from "@/cart/actions";
 
 export function ChangeQuantityButton({
